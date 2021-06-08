0add6800eba55e31c567fc5a4fbfc5bd4cf4950b
ojaynico <ojaynico@gmail.com>
2021-06-08 10:18:05 +0300

    Updated library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    kotlin("js") version "1.5.0"
+    kotlin("js") version "1.5.10"
     id("maven-publish")
     id("io.codearte.nexus-staging") version "0.30.0"
     signing
 }
 
 group = "com.github.ojaynico"
-version = "1.0.4"
+version = "1.0.5"
 
 val artifactName = project.name
 val artifactGroup = project.group.toString()
@@ -43,7 +43,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-js"))
-    implementation(npm("@react-native-async-storage/async-storage", "1.15.2"))
+    implementation(npm("@react-native-async-storage/async-storage", "1.15.5"))
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
